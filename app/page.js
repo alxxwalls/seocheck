2d94d28095458ef3d8039c2ff8423c758086dccd
alxxwalls <alex.walls@lekker.marketing>
2025-08-15 14:53:01 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,3 @@
 export default function Page() {
-  return (
-    <div style={{ padding: 24 }}>
-      SEO Checker API is deployed. Try <code>/api/check</code>
-    </div>
-  );
+  return <div style={{ padding: 24 }}>SEO Checker API is deployed. Try <code>/api/check</code></div>;
 }
